4db24ea381c181c4d7b416e37874447f1cae8c1c
Yunchou Li <liyunchou@baixing.net>
2015-01-20 00:09:09 +0800

    Basic Structure

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: YCLI
+ * Date: 2015/1/19
+ * Time: 23:30
+ */
+
+private $route_map = [
+    '/home' => "Home_Controller",
+    '/login' => "Login_Controller",
+    '/user' => "User_Controller",
+    '/news' => "News_Controller",
+
+];
+
